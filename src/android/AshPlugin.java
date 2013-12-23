66f6ea07d4dc559fefece36c12be75e70d07533a
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2013-12-23 10:17:50 +0100

    fix the api package

--- src/android/AshPlugin.java
+++ src/android/AshPlugin.java
@@ -1,7 +1,7 @@
 package pl.ug.ash;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
